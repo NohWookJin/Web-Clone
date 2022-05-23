2c005f9df9790716868dbe8315893e657692c31f
NohWookJin <jinn98@kakao.com>
2022-05-23 19:17:54 +0900

    Header.js
    분할

--- untitle/src/components/Header.js
+++ untitle/src/components/Header.js
@@ -1,19 +1,21 @@
-// function Header() {
-//     return (
-//         <div className="mainHeader">
-//           <div className="mainHeaderLogo">logo</div>
-//           <div className="mainHeaderMenu">
-//             <span><a href="#">공식카페</a></span>
-//             <span><a href="#">가이드</a></span>
-//             <span><a href="#">게임소개</a></span>
-//             <span><a href="#">자료실</a></span>
-//             <span><a href="#">고객센터</a></span>
-//           </div>
-//           <div className="mainHeaderProfile">
-//             <span></span>
-//           </div>
-//         </div>
-//     )
-// }
+function Header(props) {
+    return (
+        <div className="mainHeader">
+          <div className="mainHeaderLogo">
+            <a href="#"><img src={props.logo} alt="logo" /></a>
+          </div>
+          <div className="mainHeaderMenu">
+            <span><a href="#">공식카페</a></span>
+            <span><a href="#">가이드</a></span>
+            <span><a href="#">게임소개</a></span>
+            <span><a href="#">자료실</a></span>
+            <span><a href="#">고객센터</a></span>
+          </div>
+          <div className="mainHeaderButton">
+            <span>GAME START</span>
+          </div>
+        </div>
+    )
+}
 
-// export default Header;
+export default Header;
